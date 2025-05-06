f2c7f7b4927233c8d7ffcee347d9ff1a67adb717
liwenbo <liwenbo4922@163.com>
2025-05-06 15:59:33 +0800

    initial

---
+++ src/test/java/org/example/testjenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
